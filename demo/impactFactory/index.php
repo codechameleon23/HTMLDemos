5fa5df6163088125a60b40b07da853c2ab463d5e
Bhargav Patel <bhargav.patel0223@gmail.com>
2018-02-02 13:23:33 +0530

    index.php

---
+++ demo/impactFactory/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<title>Impact Factory</title>
+</head>
+<!-- Include All external CSS Starts -->
+<?php include 'includes/allcss.php';?>
+<!-- Include All external CSS Ends -->
+</head>
+
+<body>
+<!-- Common Header Starts -->
+<?php include 'includes/header.php';?>
+<!-- Common Header Endss -->
+
+<!-- Main content Starts -->
+<div class="main">
+    
+    <!-- S: bio-section-->
+    <?php include 'includes/pagebanner/home-banner.php';?>
+    <!-- E: bio-section-->
+
+    <!-- S: bio-section-->
+    <?php include 'includes/sections/bio-section.php';?>
+    <!-- E: bio-section-->
+
+    <!-- S: bio-section-->
+    <?php include 'includes/briefContent/brief-wrapper.php';?>
+    <!-- E: bio-section-->
+
+    <!-- S: services Section -->
+    <?php include 'includes/services/we-can-help.php';?>
+    <!-- E: Services Section -->
+
+    <!-- S : Client Section -->
+    <?php include 'includes/sections/client-section.php';?>
+    <!-- E : Client Section -->
+
+    <!-- S: brief-section-->
+    <?php include 'includes/briefContent/our-people-brief.php';?>
+    <!-- E: brief-section-->
+
+    <!-- S : Clients Section -->
+    <?php include 'includes/sections/want-to-contact.php';?>
+    <!-- E : Clients Section -->
+
+    <!-- S: blog-listing Section -->
+    <?php include 'includes/listings/blog-listing.php';?>
+    <!-- E: blog-listing Section -->
+
+
+
+
+    
+</div>
+<!-- Main content Ends --> 
+
+<!-- Common Footer Starts -->
+<?php include 'includes/footer.php';?>
+<!-- Common Footer Ends -->
+</body>
+</html>
