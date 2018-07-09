2ff39cdb6cc2875f904346de94dd7d52c7e21902
Bhargav Patel <bhargav.patel0223@gmail.com>
2018-07-09 22:20:27 +0530

    Add files via upload

--- demo/rulefesh_demo/js/scripts.js
+++ demo/rulefesh_demo/js/scripts.js
@@ -4,6 +4,8 @@ $(function(){
     $.scrollify({
         section : ".section",
         updateHash: true,
+        easing: "easeOutExpo",
+        
         before:function(index) {
     
             pageActive( $.scrollify.current() );
@@ -19,28 +21,58 @@ $(function(){
         }
     });
 
+    /*Got to slide*/
+    $('.goto_button').on('click', function(){
+        slideName = $(this).attr('href');
+        $.scrollify.instantMove(slideName);
+    });
+    /*Next slide*/
+    $('.next_button').on('click', function(){
+        $.scrollify.next();
+    });
+    /*Prev slide*/
+    $('.prev_button').on('click', function(){
+        $.scrollify.previous();
+    });
+
 
     /* Popup */
     $('.popup').popup({
         scrolllock : true,
-        //color : '#fff',
-        opacity : '.4'
+        opacity : '.4',
+        onopen: function() {
+            $.scrollify.disable();
+        },
+        onclose: function() {
+            $.scrollify.enable();
+        }
+
     });
     /* Privacy Policy poppup*/
-    $('#privacypolicy_popup').popup({});
+    $('#privacypolicy_popup').popup({
+        onopen: function() {  
+            var content =$('#privacypolicy_popup').find('.popup-body').attr('data-content')
+            $('#privacypolicy_popup').find('.popup-body').load(content);
+        }
+    });
 
 });
 
 
 function pageActive(currentSlide){
     currentSlide = $.scrollify.current();
+    var navactive = currentSlide.attr('data-nav');
     var pageThene = currentSlide.attr('data-theme');
     var slideFooter = currentSlide.attr('data-footer');
 
     $('body').attr({'data-theme':''}).attr({
-        'data-theme':pageThene,
+        'data-theme': pageThene ? pageThene : '',
         'data-footer': slideFooter ? slideFooter : "default_footer",
+        'data-nav': navactive ? navactive : ''
     });
+
+    $('.navbar li').removeClass('text_highlight');
+    $('.navbar li.'+navactive).addClass('text_highlight');
 
     $('#fullpage >  section').removeClass('active');
     currentSlide.addClass('active');
