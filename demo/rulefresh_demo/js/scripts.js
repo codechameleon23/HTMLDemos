c4fb8a84fbe7f70342595764c471dac05f466211
Bhargav Patel <bhargav.patel0223@gmail.com>
2018-07-16 21:46:19 +0530

    Add files via upload

--- demo/rulefresh_demo/js/scripts.js
+++ demo/rulefresh_demo/js/scripts.js
@@ -225,11 +225,16 @@ $(function() {
 		})
 		.done(function(response) {
 			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
-
+            //$(formMessages).removeClass('error');
+            //$(formMessages).addClass('success');
+            $(formMessages).fadeIn();
+		
 			// Set the message text.
-			$(formMessages).text(response);
+            $(formMessages).text(response);
+            
+            setTimeout(function () {
+                $(formMessages).fadeOut();
+            }, 2000);
 
 			// Clear the form.
 			$('#name').val('');
@@ -239,15 +244,21 @@ $(function() {
 		})
 		.fail(function(data) {
 			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
+			// $(formMessages).removeClass('success');
+            // $(formMessages).addClass('error');
+            
+            $(formMessages).fadeIn();
 
 			// Set the message text.
 			if (data.responseText !== '') {
 				$(formMessages).text(data.responseText);
 			} else {
 				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
+            }
+            
+            setTimeout(function () {
+                $(formMessages).fadeOut();
+            }, 2000);
 		});
 
 	});
