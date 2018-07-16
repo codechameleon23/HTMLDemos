4e3afd6cb094a43a64e6419143e2ab9b2567694d
Bhargav Patel <bhargav.patel0223@gmail.com>
2018-07-16 21:21:30 +0530

    Update mail.php

--- demo/rulefresh_demo/mail.php
+++ demo/rulefresh_demo/mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "bhargav.patel0223@gmail.com";
+        $recipient = "hello@rulefresh.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
