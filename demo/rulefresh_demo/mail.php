a2342b57fc8c33da7a4ff4458d2d060b029f3c01
Bhargav Patel <bhargav.patel0223@gmail.com>
2018-07-16 21:07:53 +0530

    Add files via upload

--- demo/rulefresh_demo/mail.php
+++ demo/rulefresh_demo/mail.php
@@ -1,47 +1,51 @@
 <?php
 
+    // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        # FIX: Replace this email with recipient email
-        $mail_to = "bhargav.pate0223@gmail.com";
-        
-        # Sender Data
-        $subject = trim($_POST["subject"]);
-        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $phone = trim($_POST["phone"]);
         $message = trim($_POST["message"]);
-        
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
-            # Set a 400 (bad request) response code and exit.
+
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($phone) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
-        
-        # Mail Content
-        $content = "Name: $name\n";
-        $content .= "Email: $email\n\n";
-        $content .= "Phone: $phone\n";
-        $content .= "Message:\n$message\n";
 
-        # email headers.
-        $headers = "From: $name <$email>";
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
+        $recipient = "hello@rulefresh.com";
 
-        # Send the email.
-        $success = mail($mail_to, $subject, $content, $headers);
-        if ($success) {
-            # Set a 200 (okay) response code.
+        // Set the email subject.
+        $subject = "New contact from $name";
+
+        // Build the email content.
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Phone: $phone\n\n";
+        $email_content .= "Message:\n$message\n";
+
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
-            # Set a 500 (internal server error) response code.
+            // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong, we couldn't send your message.";
+            echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
-        # Not a POST request, set a 403 (forbidden) response code.
+        // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
