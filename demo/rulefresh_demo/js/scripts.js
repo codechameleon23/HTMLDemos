1f4b495ad5bbf4e6a7e6590f7f711dcebedd79f3
Bhargav Patel <bhargav.patel0223@gmail.com>
2018-07-16 20:17:26 +0530

    Add files via upload

--- demo/rulefresh_demo/js/scripts.js
+++ demo/rulefresh_demo/js/scripts.js
@@ -197,3 +197,48 @@ function pageActive(currentSlide){
     $('#fullpage >  section').removeClass('active');
     currentSlide.addClass('active');
 }
+
+
+
+/*Submit contact us form*/
+(function ($) {
+    'use strict';
+
+    var form = $('.contact__form'),
+        message = $('.contact__msg'),
+        form_data;
+
+    // Success function
+    function done_func(response) {
+        alert('success');
+        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+        message.text(response);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+        form.find('input:not([type="submit"]), textarea').val('');
+    }
+
+    // fail function
+    function fail_func(data) {
+        alert('fail');
+        message.fadeIn().removeClass('alert-success').addClass('alert-success');
+        message.text(data.responseText);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+    }
+    
+    form.submit(function (e) {
+        e.preventDefault();
+        form_data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: form_data
+        })
+        .done(done_func)
+        .fail(fail_func);
+    });
+    
+})(jQuery);
