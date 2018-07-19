3ff230302b8453509160b29a2821a3a877a4fac8
Bhargav Patel <bhargav.patel0223@gmail.com>
2018-07-19 16:13:39 +0530

    Add files via upload

--- demo/rulefresh_demo/mail.php
+++ demo/rulefresh_demo/mail.php
@@ -3,14 +3,22 @@
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $phone = trim($_POST["phone"]);
-        $message = trim($_POST["message"]);
+		if(isset($_POST["name"]))
+        {	$name = strip_tags(trim($_POST["name"]));
+			$name = str_replace(array("\r","\n"),array(" "," "),$name);
+		}
+		
+		if(isset($_POST["email"]))
+			$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+	
+		if(isset($_POST["phone"]))
+			$phone = trim($_POST["phone"]);
+		
+		if(isset($_POST["message"]))
+			$message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($phone) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
