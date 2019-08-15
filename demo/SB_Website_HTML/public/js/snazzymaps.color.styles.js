9772a318d26c6a31b0c832f416775bc9a5ea2915
Bhargav Patel <bhargav.patel0223@gmail.com>
2019-08-15 13:12:31 +0530

    SB_Website_HTML v1

---
+++ demo/SB_Website_HTML/public/js/snazzymaps.color.styles.js
@@ -0,0 +1,89 @@
+var snazzymapsColorStyle = [{
+	featureType: "all",
+	elementType: "labels.text.fill",
+	stylers: [{
+		color: "#ffffff"
+	}]
+}, {
+	featureType: "all",
+	elementType: "labels.text.stroke",
+	stylers: [{
+		visibility: "on"
+	}, {
+		color: "#3e606f"
+	}, {
+		weight: 2
+	}, {
+		gamma: .84
+	}]
+}, {
+	featureType: "all",
+	elementType: "labels.icon",
+	stylers: [{
+		visibility: "off"
+	}]
+}, {
+	featureType: "landscape",
+	elementType: "geometry",
+	stylers: [{
+		color: "#31b7bc"
+	}, {
+		lightness: "78"
+	}]
+}, {
+	featureType: "poi",
+	elementType: "geometry",
+	stylers: [{
+		color: "#00656c"
+	}, {
+		lightness: "7"
+	}, {
+		saturation: "94"
+	}, {
+		gamma: "1.18"
+	}]
+}, {
+	featureType: "poi.park",
+	elementType: "geometry",
+	stylers: [{
+		color: "#73d8df"
+	}, {
+		lightness: "4"
+	}, {
+		saturation: "33"
+	}, {
+		gamma: "1.18"
+	}]
+}, {
+	featureType: "road",
+	elementType: "geometry",
+	stylers: [{
+		color: "#00656c"
+	}, {
+		lightness: "0"
+	}, {
+		saturation: "-1"
+	}, {
+		gamma: "1.18"
+	}]
+}, {
+	featureType: "transit",
+	elementType: "geometry",
+	stylers: [{
+		color: "#31b7bc"
+	}, {
+		lightness: "0"
+	}, {
+		saturation: "0"
+	}, {
+		gamma: "1"
+	}]
+}, {
+	featureType: "water",
+	elementType: "geometry",
+	stylers: [{
+		color: "#31b7bc"
+	}, {
+		lightness: "18"
+	}]
+}]
