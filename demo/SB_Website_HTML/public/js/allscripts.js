68b6e678b3e10feffbde08fb049769a8a5831019
Bhargav Patel <bhargav.patel0223@gmail.com>
2019-08-16 20:35:14 +0530

    feedbacls 2

--- demo/SB_Website_HTML/public/js/allscripts.js
+++ demo/SB_Website_HTML/public/js/allscripts.js
@@ -99,6 +99,34 @@ function filtered(fp) {
   }
 }
 
+// Inview js
+var $animation_elements = $('.in-view-animation');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function (i, el) {
+    var $element = $(el);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position - 10)) {
+      // $element.addClass('in-view add-animation');
+      setTimeout(function () {
+        $(el).addClass('in-view add-animation');
+      }, 100 + (i * 50));
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
 $(document).ready(function () {
 
   // Navbar toggle
