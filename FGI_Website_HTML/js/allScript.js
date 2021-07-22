f0b8f8b3da63fc456fd0c878541081db5060f514
Bhargav Patel <bhargav.patel0223@gmail.com>
2021-07-22 23:09:30 +0530

    FGI_Website_HTML: fix js error

--- FGI_Website_HTML/js/allScript.js
+++ FGI_Website_HTML/js/allScript.js
@@ -210,23 +210,23 @@ $('table.stacked').stackedRows();
 //  -------------------------------------------
 //  Set active on scroll
 //  -------------------------------------------
-  var mainNavLinks = document.querySelectorAll("nav ul li a");
-  var mainSections = document.querySelectorAll("main section");
-
-  var lastId;
-  var cur = [];
-  window.addEventListener("scroll", function (event) {
-    var fromTop = window.scrollY + 10;
-    Array.prototype.slice.call(mainNavLinks).forEach(function (link) {
-      var section = document.querySelector(link.hash);
-
-      if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
-        link.classList.add("current");
-      } else {
-        link.classList.remove("current");
-      }
-    });
-  });
+//   var mainNavLinks = document.querySelectorAll("nav ul li a");
+//   var mainSections = document.querySelectorAll("main section");
+//
+//   var lastId;
+//   var cur = [];
+//   window.addEventListener("scroll", function (event) {
+//     var fromTop = window.scrollY + 10;
+//     Array.prototype.slice.call(mainNavLinks).forEach(function (link) {
+//       var section = document.querySelector(link.hash);
+//
+//       if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+//         link.classList.add("current");
+//       } else {
+//         link.classList.remove("current");
+//       }
+//     });
+//   });
 });
 //  -------------------------------------------
 //  iPhone page refresh on browser back button
