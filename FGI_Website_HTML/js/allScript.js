9c93e250efe200caf22e0910be80dc9419306590
Bhargav Patel <bhargav.patel0223@gmail.com>
2021-07-22 22:59:05 +0530

    FGI_Website_HTML: file attched indication

--- FGI_Website_HTML/js/allScript.js
+++ FGI_Website_HTML/js/allScript.js
@@ -186,6 +186,28 @@ $('table.stacked').stackedRows();
 
 
 //  -------------------------------------------
+//  Shoe Attached file name
+//  -------------------------------------------
+  if($(".fileInput").length > 0){
+    $(".fileInput").change(function() {
+      var fileInput = $(this);
+      var parent = fileInput.closest(".fileGroup");
+      var fileName = fileInput[0].files[0].name;
+      parent.find(".removeFile .fileName").text(fileName);
+      parent.addClass("attached");
+    });
+  }
+  if($(".removeFile").length > 0){
+    $(".removeFile").on("click", function() {
+      var removeFile = $(this);
+      var parent = removeFile.closest(".fileGroup");
+      var fileInput = parent.find(".fileInput");
+      fileInput.val("");
+      removeFile.find(".fileName").text("");
+      parent.removeClass("attached");
+    });
+  }
+//  -------------------------------------------
 //  Set active on scroll
 //  -------------------------------------------
   var mainNavLinks = document.querySelectorAll("nav ul li a");
