22b93a0d85aa86aff03a1bb044cb6aafe9317d40
Bhargav Patel <bhargav.patel0223@gmail.com>
2021-08-12 00:44:56 +0530

    DBA_Africa_Website_HTML: desktop feedback

--- DBA_Africa_Website_HTML/js/allScript.js
+++ DBA_Africa_Website_HTML/js/allScript.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
       center: true,
       items: 1,
       stagePadding: 0,
-      margin: 0,
+      margin: 2,
       loop: true,
       mouseDrag: true,
       nav: false,
@@ -89,15 +89,9 @@ $(document).ready(function () {
       // animateIn: isIE11 || isIE10 ? "" : "fadeIn",
       // animateOut: isIE11 || isIE10 ? "" : "fadeOut",
       smartSpeed: 500,
-      autoplay: true,
+      // autoplay: true,
       autoplayTimeout: 4000,
       autoplayHoverPause: true,
-    });
-    $('.jsCarouselPrv').on('click', function () {
-      $(this).closest('.carousel-outer').find(bannerCarousel).trigger('prev.owl.carousel');
-    });
-    $('.jsCarouselNxt').on('click', function () {
-      $(this).closest('.carousel-outer').find(bannerCarousel).trigger('next.owl.carousel');
     });
   };
 
@@ -128,14 +122,21 @@ $(document).ready(function () {
         },
       }
     });
-    $('.jsCarouselPrv').on('click', function () {
-      $(this).closest('.carousel-outer').find(bannerCarouselCenter).trigger('prev.owl.carousel');
-    });
-    $('.jsCarouselNxt').on('click', function () {
-      $(this).closest('.carousel-outer').find(bannerCarouselCenter).trigger('next.owl.carousel');
-    });
   };
 
+  $('.jsCarouselPrv, .jsCarouselNxt').on('click', function () {
+    var trigger = $(this);
+    var targetCarousel =  trigger.closest('.carousel-outer').find(bannerCarousel);
+    var targetCarouselCenter = trigger.closest('.carousel-outer').find(bannerCarouselCenter);
+    if(trigger.hasClass('jsCarouselPrv')){
+      targetCarousel.trigger('prev.owl.carousel');
+      targetCarouselCenter.trigger('prev.owl.carousel');
+    }
+  if(trigger.hasClass('jsCarouselNxt')){
+    targetCarousel.trigger('next.owl.carousel');
+    targetCarouselCenter.trigger('next.owl.carousel');
+    }
+  });
 
 //  -------------------------------------------
 //  Magnific Popup : Gallery
