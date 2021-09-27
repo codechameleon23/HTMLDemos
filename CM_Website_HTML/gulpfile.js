fff17166d9d88511475c86c3463a59d19b571474
Bhargav Patel <bhargav.patel0223@gmail.com>
2021-09-27 16:42:29 +0530

    CM_Website_HTML: property page

--- CM_Website_HTML/gulpfile.js
+++ CM_Website_HTML/gulpfile.js
@@ -69,7 +69,7 @@ const PROJECT_JS_LIST = [
   SRC_JS_PATH+'/_popup.js',
   SRC_JS_PATH+'/_magnificPopup.js',
   SRC_JS_PATH+'/_aos.js',
-  // SRC_JS_PATH+'/_accordion.js',
+  SRC_JS_PATH+'/_accordion.js',
   // SRC_JS_PATH+'/_galleryFilter.js',
   // SRC_JS_PATH+'/_tableStackedRows.js',
   SRC_JS_PATH+'/_inView.js',
