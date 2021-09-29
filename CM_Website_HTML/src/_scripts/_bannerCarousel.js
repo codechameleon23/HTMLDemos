39cf742d22864ef7eb34617852ca8bb8c06f4953
Bhargav Patel <bhargav.patel0223@gmail.com>
2021-09-29 10:37:03 +0530

    CM_Website_HTML: feedback 1 in progress

---
+++ CM_Website_HTML/src/_scripts/_bannerCarousel.js
@@ -0,0 +1,48 @@
+//  -------------------------------------------
+//  Banner carousel
+//  -------------------------------------------
+  if ($(".jsBannerCarousel").length > 0) {
+    $(".jsBannerCarousel").each(function () {
+      var jsBannerCarousel = $(this);
+      var carouselOuter = jsBannerCarousel.closest('.carousel-outer');
+      var jsBannerCarouselPrv = carouselOuter.find('.jsCarouselPrv');
+      var jsBannerCarouselNxt = carouselOuter.find('.jsCarouselNxt');
+      var isBannerCarouselSingle = jsBannerCarousel.children().length === 1;
+      jsBannerCarousel.owlCarousel({
+        center: true,
+        items: 1,
+        stagePadding: 0,
+        margin: 1,
+        loop: !isBannerCarouselSingle,
+        mouseDrag: !isBannerCarouselSingle,
+        touchDrag: !isBannerCarouselSingle,
+        pullDrag: !isBannerCarouselSingle,
+        nav: false,
+        dots: true,
+        // animateIn: isIE11 || isIE10 ? "" : "fadeIn",
+        // animateOut: isIE11 || isIE10 ? "" : "fadeOut",
+        smartSpeed: 500,
+        // autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+      });
+      if(!isBannerCarouselSingle){
+        if (jsBannerCarouselPrv) {
+          jsBannerCarouselPrv.on('click', function () {
+            carouselOuter.find(jsBannerCarousel).trigger('prev.owl.carousel');
+          });
+        }
+        if (jsBannerCarouselNxt) {
+          jsBannerCarouselNxt.on('click', function () {
+            carouselOuter.find(jsBannerCarousel).trigger('next.owl.carousel');
+          });
+        }
+      }
+      if(isBannerCarouselSingle){
+        jsBannerCarouselPrv.css({ display: "none" });
+        jsBannerCarouselNxt.css({ display: "none" });
+      }
+
+    })
+  }
+
