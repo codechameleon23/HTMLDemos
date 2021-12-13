7094525fd64f7012f85384be6fad38750a115fad
Bhargav Patel <bhargav.patel0223@gmail.com>
2021-12-13 19:51:43 +0530

    BB_Website_HTML: custom number input

--- BB_Website_HTML/src/_scripts/_customSelectDropdown.js
+++ BB_Website_HTML/src/_scripts/_customSelectDropdown.js
@@ -3,13 +3,11 @@
 //  -------------------------------------------
 if ($(".jsCustomSelectDropdown").length > 0) {
   $(".jsCustomSelectDropdown").each(function () {
-    var selecElm = $(this);
-    var $this = selecElm,
-      numberOfOptions = selecElm.children("option").length;
-    var classesList = selecElm
+    var $this = $(this);
+      numberOfOptions = $this.children("option").length;
+    var classesList = $this
       .attr("class")
       .replace("jsCustomSelectDropdown", "custom-select-styled select");
-    console.log("classesList---------------", classesList);
     $this.addClass("select-hidden");
     $this.wrap('<div class="custom-select-wrapper"></div>');
     $this.after('<div class="' + classesList + '"></div>');
@@ -62,3 +60,56 @@ if ($(".jsCustomSelectDropdown").length > 0) {
     });
   });
 }
+
+//  -------------------------------------------
+//  Custom Nume inout
+//  -------------------------------------------
+
+if ($(".jsCustomNumberInput").length > 0) {
+    $(".jsCustomNumberInput").each(function () {
+      var numInputElm = $(this);
+      numInputElm.addClass('custom-number').append(
+        '<div class="quantity-nav">'+
+            '<div class="quantity-button quantity-up icon">'+
+                '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">'+
+                    '<path d="m396.6 352 19.4-20.7L256 160 96 331.3l19.3 20.7L256 201.5z"/>'+
+                '</svg>'+
+            '</div>'+
+            '<div class="quantity-button quantity-down icon">'+
+                '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">'+
+                    '<path d="m396.6 160 19.4 20.7L256 352 96 180.7l19.3-20.7L256 310.5z"/>'+
+                '</svg>'+
+            '</div>'+
+        '</div>'
+      );
+      var spinner = numInputElm,
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find(".quantity-up"),
+        btnDown = spinner.find(".quantity-down"),
+        min = input.attr("min"),
+        max = input.attr("max");
+  
+      btnUp.click(function () {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+  
+      btnDown.click(function () {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+    });
+  }
+  
