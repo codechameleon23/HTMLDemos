e99a07e34e1a85bf3a3eef9026dac6fb4fd24599
Bhargav Patel <bhargav.patel0223@gmail.com>
2021-12-20 02:05:10 +0530

    NF_Summit_Website_HTML: final touches

--- NF_Summit_Website_HTML/js/allScript.js
+++ NF_Summit_Website_HTML/js/allScript.js
@@ -84,22 +84,33 @@ $(document).ready(function () {
 //  -------------------------------------------
 //  Simple tabs with Select dropdown
 //  -------------------------------------------
-if($(".tabs-collection").length > 0){
-   var tabCollections = $(".tabs-collection");
-   tabCollections.each(function(){
-      var tabCollection = $(this);
-      var tabTriggers = tabCollection.find('.tab');
-      var tabTargetAll = tabCollection.find('[data-tab]');
-      tabTriggers.on('click', function(){
-         if(getViewportWidth() < 576 && $(this).hasClass('active')){
-            tabTargetAll.removeClass('active');   
-         }else{
-            tabTargetAll.removeClass('active');
-            tabCollections.find('[data-tab="'+$(this).data('tab')+'"]').addClass('active');
-         }
-      })
-   })
+if ($(".tabs-collection").length > 0) {
+  var tabCollections = $(".tabs-collection");
+  tabCollections.each(function () {
+    var tabCollection = $(this);
+    var tabTriggers = tabCollection.find(".tab");
+    var tabTargetAll = tabCollection.find("[data-tab]");
+    tabTriggers.on("click", function () {
+      if (getViewportWidth() < 711 && $(this).hasClass("active")) {
+        tabTargetAll.removeClass("active");
+      } else {
+        tabTargetAll.removeClass("active");
+        tabCollections
+          .find('[data-tab="' + $(this).data("tab") + '"]')
+          .addClass("active");
+      }
+    });
+    $(window).on("resize", function () {
+      if (
+        getViewportWidth() > 711 &&
+        tabCollection.find(".active[data-tab]").length === 0
+      ) {
+        tabTriggers.eq(0).click();
+      }
+    });
+  });
 }
+
 
 //  -------------------------------------------
 //  Set active on scroll
