b02012195e28700399245b8e8d9815afbf45d9ca
Bhargav Patel <bhargav.patel0223@gmail.com>
2022-04-28 12:45:57 +0530

    CB_Website_HTML: index page

---
+++ CB_Website_HTML/src/_scripts/_aos.js
@@ -0,0 +1,9 @@
+//  -------------------------------------------
+//  Animate on scroll
+//  -------------------------------------------
+  AOS.init({
+    duration: 1000,
+    once: true,
+    // easing: 'ease-in-out',
+    anchorPlacement: 'center-bottom',
+  });
---
+++ CB_Website_HTML/src/_scripts/_snazzymaps.color.styles.js
@@ -0,0 +1,134 @@
+var snazzymapsColorStyle = [
+  {
+    featureType: "all",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#ffffff",
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        visibility: "on",
+      },
+      {
+        color: "#3e606f",
+      },
+      {
+        weight: 2,
+      },
+      {
+        gamma: 0.84,
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "landscape",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#405769",
+      },
+      {
+        lightness: "0",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#0B303E",
+      },
+      // {
+      //   lightness: "7",
+      // },
+      // {
+      //   saturation: "94",
+      // },
+      // {
+      //   gamma: "1.18",
+      // },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#0B303E",
+      },
+      // {
+      //   lightness: "4",
+      // },
+      // {
+      //   saturation: "33",
+      // },
+      // {
+      //   gamma: "1.18",
+      // },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#1d2328",
+      },
+      // {
+      //   lightness: "0",
+      // },
+      // {
+      //   saturation: "-1",
+      // },
+      // {
+      //   gamma: "1.18",
+      // },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#31b7bc",
+      },
+      {
+        lightness: "0",
+      },
+      {
+        saturation: "0",
+      },
+      {
+        gamma: "1",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#003c59",
+      },
+      // {
+      //   lightness: "18",
+      // },
+    ],
+  },
+];
