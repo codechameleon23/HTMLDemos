44a39af546b5ed038613dde57b981f1065bbc349
Bhargav Patel <bhargav.patel0223@gmail.com>
2022-08-24 01:13:00 +0530

    WCS_Website_HTML: initial homepage

---
+++ WCS_Website_HTML/src/_scripts/_customInputTypeFile.js
@@ -0,0 +1,26 @@
+//  -------------------------------------------
+//  Custom File input
+//  -------------------------------------------
+var inputs = document.querySelectorAll(".inputfile");
+Array.prototype.forEach.call(inputs, function (input) {
+  var label = input.nextElementSibling,
+    labelVal = label.innerHTML;
+  input.addEventListener("change", function (e) {
+    var fileName = "";
+    if (this.files && this.files.length > 1) {
+      fileName = (this.getAttribute("data-multiple-caption") || "").replace(
+        "{count}",
+        this.files.length
+      );
+    } else {
+      fileName = e.target.value.replace(/^.*[\\\/]/, "");
+    }
+
+    if (fileName) {
+      console.log('fileName', fileName);
+      label.querySelector("span").innerHTML = fileName;
+    } else {
+      label.innerHTML = labelVal;
+    }
+  });
+});
